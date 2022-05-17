f6b79802a9704657a5876362d9229ae2230f08f0
BasawarajBiradar <basawarajbiradar007@gmail.com>
2022-05-17 22:00:33 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HeadMenuComponent } from './head-menu/head-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { FootMenuComponent } from './foot-menu/foot-menu.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { ItemComponent } from './item/item.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeadMenuComponent,
+    FooterComponent,
+    FootMenuComponent,
+    MainContainerComponent,
+    ListItemsComponent,
+    ItemComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
